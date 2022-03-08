8a569000a9adbcad40417546f9a2e655ef3e0b2e
David Chiu <davidchiu0812@gmail.com>
2022-03-09 00:05:52 +1100

    first commit

--- src/components/Release.jsx
+++ src/components/Release.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { BsArrowRight } from "react-icons/bs";
 import release1 from "../assets/release1.png";
 import release11 from "../assets/release11.png";
-import release2 from "../assets/release2.png";
-import Card from "./Card";
 
 export default function Release() {
   return (
